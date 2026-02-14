a1647e1982f65e4fcd83da5c10a3e43bf49f1cc6
5arSanti <santiari05@hotmail.com>
2026-02-13 22:07:01 -0500

    Update appId in capacitor.config.ts to reflect new application identifier for the Ionic project, ensuring proper configuration for deployment.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionict2.app',
   appName: 'mobile-ionic-t2',
   webDir: 'www'
 };
