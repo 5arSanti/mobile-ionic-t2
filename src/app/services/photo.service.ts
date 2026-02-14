d46301603f2683831eae369c5c90ad9603b3b827
5arSanti <santiari05@hotmail.com>
2026-02-13 20:53:15 -0500

    Update angular.json to format schematicCollections and disable analytics

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  public async addNewToGalery() {
+    const capturedPhoto: Photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+  }
+}
